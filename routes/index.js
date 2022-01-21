89d1e7bc1fc2bd0527521b96be10560c772f6a9e
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2022-01-21 15:40:20 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-node_modules/
+const express - require("express")
+const router = express.Router()
+
+
+router.get("/", (req, res) => {
+  res.render("index")
+})
+
+router.get("/home", (req, res) => {
+  res.redirect("/")
+})
+module.exports router;
